627be43226c6e3d0ecd35cef6493089082e66370
voidmanevoid <voidarelli@protonmail.com>
2023-07-13 18:18:23 -0600

    void branded resume

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <b-container>
       <b-row class="mb-5">
         <b-col>
-              <h2 class="text-success">Tim Zagelow</h2>
+              <h2 class="text-success">void mane void</h2>
         </b-col>
       </b-row>
 
@@ -12,16 +12,35 @@
           <h4>About Me</h4>
           <h5><em>I am passionate about solving challenging technology problems. I hold myself to the highest standard with regards to engineering, teamwork and aesthetics.</em></h5>
           <h5 class="mt-3"><em>My interests include blockchain/web3, NFTs, freedom, weight-lifting and vinyl records.</em></h5>
-          <h5 class="mt-3"><em>Currently learning Rust with various side projects I've been working on!</em></h5>
+          <h5 class="mt-3"><em>Currently using Rust with various side projects I've been working on!</em></h5>
         </b-col>
 
         <b-col class="mb-5">
           <h4>Contact</h4>
           <b-list-group>
-            <b-list-group-item>Phone: 503-758-8384</b-list-group-item>
-            <b-list-group-item>Email: <b-link href="mailto:timzagelow@gmail.com">timzagelow@gmail.com</b-link></b-list-group-item>
-            <!-- <b-list-group-item>Code: <b-link href="https://github.com/timzagelow" target="_blank">GitHub</b-link></b-list-group-item> -->
+            <b-list-group-item>Email: <b-link href="mailto:voidmanevoid@gmail.com">voidmanevoid@gmail.com</b-link></b-list-group-item>
           </b-list-group>
+
+          <h4 class="mt-5">NFT Projects</h4>
+          <b-row>
+            <b-col>
+          <b-list-group>
+            <b-list-group-item>Cets </b-list-group-item>
+            <b-list-group-item>Turtles </b-list-group-item>
+            <b-list-group-item>Kami </b-list-group-item>
+            <b-list-group-item>Toonies </b-list-group-item>
+          </b-list-group>
+            </b-col>
+ <b-col>
+          <b-list-group>
+            <b-list-group-item>Dudes </b-list-group-item>
+            <b-list-group-item>Neo Hunters </b-list-group-item>
+            <b-list-group-item>Zero Monke Biz </b-list-group-item>
+            <b-list-group-item>Froots </b-list-group-item>
+                      </b-list-group>
+            </b-col>
+          </b-row>
+
         </b-col>
       </b-row>
 
@@ -56,7 +75,7 @@ export default {
     Skills,
   },
   created() {
-    document.title = "Tim Zagelow Resume";
+    document.title = "void mane void Resume";
   }
 }
 </script>
